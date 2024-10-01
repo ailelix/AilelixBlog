25c6ea430d2928532a9c02d8d4eb1d0e736e3c18
Ailelix <felixchen531@gmail.com>
2024-10-01 10:57:29 +0200

    Init repo

---
+++ content/index.md
@@ -0,0 +1,19 @@
+---
+title: Ailelix's Blog
+---
+
+Bonjour!
+这里是Ailelix的博客
+
+您将会看到：
+- 菜鸡运维
+- 编程苦手
+- 生活杂记
+- 不要笑挑战
+- 等等等等
+
+在开始之前：
+1. 本站由[Quartz 4.0](https://github.com/jackyzha0/quartz)驱动，感谢如此美观的框架
+2. 本站使用Google Fonts CDN，在特定网络情况下可能显示效果不佳
+3. 虽然是我自己的博客，但是叠个甲先 `请妥善使用Giscus评论`
+4. 所有文章均遵循[CC BY-NC-SA 4.0](https://creativecommons.org/licenses/by-nc-sa/4.0/)协议，请妥善转载或引用
---
+++ quartz.layout.ts
@@ -0,0 +1,51 @@
+import { PageLayout, SharedLayout } from "./quartz/cfg"
+import * as Component from "./quartz/components"
+
+// components shared across all pages
+export const sharedPageComponents: SharedLayout = {
+  head: Component.Head(),
+  header: [],
+  afterBody: [],
+  footer: Component.Footer({
+    links: {
+      "CC BY-NC-SA 4.0": "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+      "Github": "https://github.com/ccxxvv77",
+      "X": "https://x.com/Ailllelllix"
+    },
+  }),
+}
+
+// components for pages that display a single page (e.g. a single note)
+export const defaultContentPageLayout: PageLayout = {
+  beforeBody: [
+    Component.Breadcrumbs(),
+    Component.ArticleTitle(),
+    Component.ContentMeta(),
+    Component.TagList(),
+    Component.MobileOnly(Component.TableOfContents())
+  ],
+  left: [
+    Component.PageTitle(),
+    Component.MobileOnly(Component.Spacer()),
+    Component.Search(),
+    Component.Darkmode(),
+    Component.DesktopOnly(Component.Explorer())
+  ],
+  right: [
+    Component.MobileOnly(Component.Explorer()),
+    Component.DesktopOnly(Component.TableOfContents())
+  ],
+}
+
+// components for pages that display lists of pages  (e.g. tags or folders)
+export const defaultListPageLayout: PageLayout = {
+  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  left: [
+    Component.PageTitle(),
+    Component.MobileOnly(Component.Spacer()),
+    Component.Search(),
+    Component.Darkmode(),
+    Component.DesktopOnly(Component.Explorer()),
+  ],
+  right: [],
+}
